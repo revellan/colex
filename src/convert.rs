0a9c178bff01b6a53f75762f47b92f2556f6a39b
revellan <climb.simonschmidt@web.de>
2025-07-27 19:12:24 +0000

    Improve Speed

--- src/convert.rs
+++ src/convert.rs
@@ -4,25 +4,11 @@ pub fn convert(s: &mut String) {
         .next()
         .expect("Error: String doesn't contain a character!");
     let vocals: [char; 10] = ['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'];
-    let mut starts_with_vocal: bool = false;
-    for i in vocals {
-        if first_char == i {
-            starts_with_vocal = true;
-        }
-    }
-    if starts_with_vocal {
+    if vocals.contains(&first_char) {
         s.push_str("-fay");
     } else {
-        let mut boilerplate: bool = false;
-        let mut str = String::new();
-        for c in s.chars() {
-            if boilerplate {
-                str.push(c);
-            } else {
-                boilerplate = true;
-            }
-        }
-        *s = str;
-        s.push_str(&(first_char.to_string() + "ay"));
+        let first_ch_len: usize= first_char.len_utf8();
+        let string_slice = &s[first_ch_len..];
+        *s = format!("{}-{}ay", string_slice, first_char);
     }
 }
