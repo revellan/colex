730e16f8879365348d944f605ce21c3c645eb70f
revellan <climb.simonschmidt@web.de>
2025-07-28 05:27:58 +0000

    write basic clitool functionality

--- src/clitool.rs
+++ src/clitool.rs
@@ -1,3 +1,20 @@
-pub fn add() {
-    
+use std::collections::HashMap;
+pub struct Employee {
+    pub name: String,
+}
+impl Employee {
+    pub fn new(name: &str) -> Employee {
+        Employee {
+            name: String::from(name),
+        }
+    }
+    pub fn add(self, dep: &str) {
+        let mut company: HashMap<&str, &str> = HashMap::new();
+        company.contains_key::<str>(&self.name);
+        company.insert(&self.name, dep);
+    }
+    pub fn list_dep(self) -> Vec<String> {
+        let deps: Vec<String> = Vec::new();
+        deps
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,15 @@
-use colex::median;
+use colex::{clitool, median};
 fn main() {
-    _test_convert();
+    _test_hashmap();
 }
 fn _test_convert() {
     let mut string = String::new();
     print!("Enter String: ");
     use std::{io, io::Write};
     io::stdout().flush().expect("Error flushing stdout");
-    io::stdin().read_line(&mut string).expect("Failed to read line");
+    io::stdin()
+        .read_line(&mut string)
+        .expect("Failed to read line");
     string.pop();
     colex::convert::convert(&mut string);
     println!("{}", string);
@@ -18,4 +20,9 @@ fn _test_median() {
     if let (Some(median), Some(mode)) = (md.median, md.mode) {
         println!("Median: {}\nMode: {}", median, mode)
     }
+}
+fn _test_hashmap() {
+    let sally = clitool::Employee::new("Sally");
+    let s: String = String::from("Sales");
+    sally.add(&s);
 }
