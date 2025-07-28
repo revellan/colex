04f1f0377f3ba7498691ef1ba04efc1757b0e51c
revellan <climb.simonschmidt@web.de>
2025-07-28 05:50:13 +0000

    Write efficient add function to take ownership from hashmap and then give it back

--- src/clitool.rs
+++ src/clitool.rs
@@ -9,12 +9,18 @@ impl Employee {
         }
     }
     pub fn add(self, dep: &str) {
-        let mut company: HashMap<&str, &str> = HashMap::new();
-        company.contains_key::<str>(&self.name);
-        company.insert(&self.name, dep);
+        let mut company: HashMap<&str, Vec<String>> = HashMap::new();
+        let mut deps: Vec<String>;
+        match company.remove::<str>(&self.name) {
+            Some(v) => deps = v,
+            None => deps = Vec::new(),
+        }
+        deps.push(dep.to_string());
+        company.insert(&self.name, deps);
     }
     pub fn list_dep(self) -> Vec<String> {
         let deps: Vec<String> = Vec::new();
+
         deps
     }
 }
